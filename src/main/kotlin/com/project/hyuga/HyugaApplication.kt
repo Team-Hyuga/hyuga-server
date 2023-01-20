c770fca700dd23f02e53f6e2b8bcabde6f2ca8d3
Kangmin <ehfehf0629@gmail.com>
2023-01-20 13:39:22 +0900

    Init Project

---
+++ src/main/kotlin/com/project/hyuga/HyugaApplication.kt
@@ -0,0 +1,11 @@
+package com.project.hyuga
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HyugaApplication
+
+fun main(args: Array<String>) {
+    runApplication<HyugaApplication>(*args)
+}
