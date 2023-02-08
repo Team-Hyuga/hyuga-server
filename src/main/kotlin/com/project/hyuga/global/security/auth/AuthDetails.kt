0898df10a36413de1ec4a30d69f6362b15f15799
Kangmin <ehfehf0629@gmail.com>
2023-02-08 23:53:39 +0900

    chore : AuthDetails

--- src/main/kotlin/com/project/hyuga/global/security/auth/AuthDetails.kt
+++ src/main/kotlin/com/project/hyuga/global/security/auth/AuthDetails.kt
@@ -1,16 +1,20 @@
 package com.project.hyuga.global.security.auth
 
+import com.project.hyuga.domain.user.domain.User
 import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 
-class AuthDetails : UserDetails {
+class AuthDetails(
+    private val user: User
+) : UserDetails {
 
     override fun getAuthorities(): MutableCollection<out GrantedAuthority> =
-        TODO("Not yet implemented")
+        mutableListOf(SimpleGrantedAuthority(user.authority.name))
 
     override fun getPassword(): String? = null
 
-    override fun getUsername(): String = TODO("Not yet implemented")
+    override fun getUsername(): String = user.email
 
     override fun isAccountNonExpired(): Boolean = true
 
