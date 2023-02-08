785c581f6277cdc7427ce255d16989a704a01415
Kangmin <ehfehf0629@gmail.com>
2023-02-08 23:56:43 +0900

    chore : AuthDetailsService

--- src/main/kotlin/com/project/hyuga/global/security/auth/AuthDetailsService.kt
+++ src/main/kotlin/com/project/hyuga/global/security/auth/AuthDetailsService.kt
@@ -1,14 +1,19 @@
 package com.project.hyuga.global.security.auth
 
+import com.project.hyuga.domain.user.domain.repository.UserRepository
+import com.project.hyuga.domain.user.exception.UserNotFoundException
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Component
 
 @Component
-class AuthDetailsService : UserDetailsService {
+class AuthDetailsService(
+    private val userRepository: UserRepository
+) : UserDetailsService {
 
-    override fun loadUserByUsername(username: String?): UserDetails {
-        TODO("Not yet implemented")
+    override fun loadUserByUsername(username: String): UserDetails {
+        val user = userRepository.findByEmail(username) ?: throw UserNotFoundException
+        return AuthDetails(user)
     }
 
 }
