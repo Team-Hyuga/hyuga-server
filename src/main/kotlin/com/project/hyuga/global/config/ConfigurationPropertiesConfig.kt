c10c086143d3e23ac6808ea11ff879dd7d56383b
Kangmin <ehfehf0629@gmail.com>
2023-02-13 01:32:17 +0900

    chore : 빈 등록

--- src/main/kotlin/com/project/hyuga/global/config/ConfigurationPropertiesConfig.kt
+++ src/main/kotlin/com/project/hyuga/global/config/ConfigurationPropertiesConfig.kt
@@ -1,7 +1,9 @@
 package com.project.hyuga.global.config
 
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.context.annotation.Configuration
 
+@Configuration
 @ConfigurationPropertiesScan("com.project.hyuga")
 class ConfigurationPropertiesConfig {
 }
